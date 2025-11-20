53a462bd2f4ae58496207db19a36d3997d9bf641
user_nguyen_le_khuong <nguyen_le_khuong@optimizer.co.jp>
2025-11-20 19:54:29 +0700

    minit

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://oto-giakhanh.vercel.app",
+  generateRobotsTxt: true, // Tự tạo robots.txt
+  sitemapSize: 7000,
+};
